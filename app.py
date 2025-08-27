db7d6abd6c17ef7021104522f3ab1549f8a8e072
GiancarloFecap <giancarlo.fecap@gmail.com>
2025-08-27 13:16:38 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/usuario')
+def usuario():
+    return render_template('usuario.html')
+
+@app.route('/curso')
+def curso():
+    return render_template('curso.html')
+
+@app.route('/agendamento')
+def agendamento():
+    return render_template('agendamento.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
